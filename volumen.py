5ea929801ec24c6369684338360a234e1a0a09ad
adrian dañon <adriancanon@unisangil.edu.co>
2025-03-24 11:58:40 -0500

    perimite hallar el volumen

---
+++ volumen.py
@@ -0,0 +1,8 @@
+#laboratorio segundo ejercicio
+##adrian Cañon
+#brayan Ladino
+#andres Ramirez
+#este programa permite saber la cantidad de material para la fabricacion globos y pelotas" # esto lo hace por medio del rapdio
+radio= float(input("ingreese el radio de la pelota en cm:"))
+volumen = (4/3)*3.14159*radio**3 #esta es la formula genral para hallar el volumen de esfearas
+print("el volumen de la pelota/esfera es:", volumen,"cm3")
